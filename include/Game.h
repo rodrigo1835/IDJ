bd58fed468b1e7b0f309f78234c63ce642d5fffb
Rodrigo <rodrigo.videira.jc@gmail.com>
2025-09-03 19:28:48 -0300

    atualizações na classe game

--- include/Game.h
+++ include/Game.h
@@ -12,11 +12,17 @@ class Game {
         int width,height;
 
         static Game * instance;
+        ~Game();
         
         Game(const string& title, int width, int height);
 
         void initSDL(Uint32 flags);
+        void initIMG(Uint32 flags);
+        void initMIX(Uint32 Flags);
 
+        SDL_Window* SDL_CreateWindow(const char* title, int x, int y, int w, int h, Uint32 flags);
+        SDL_Renderer* SDL_CreateRenderer(SDL_Window* window, int index, Uint32 flags);
+        
     public:
         static Game * GetInstance(const string& title = "Game", int width = 800, int height = 600);
     
--- src/Game.cpp
+++ src/Game.cpp
@@ -14,6 +14,22 @@ Game::Game(const string& title, int width, int height) {
     this->height = height;
 
     initSDL(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
+    initIMG(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF);
+    initMIX(MIX_INIT_MP3 | MIX_INIT_OGG);
+}
+
+Game* Game::GetInstance(const string& title, int width, int height) {
+    if ( !instance )
+        instance = new Game(title, width, height);
+
+    return instance;
+}
+
+Game::~Game() {
+    Mix_CloseAudio();
+    Mix_Quit();
+    IMG_Quit();
+    SDL_Quit();
 }
 
 void Game::initSDL(Uint32 flags) {
@@ -23,9 +39,21 @@ void Game::initSDL(Uint32 flags) {
     }
 }
 
-Game* Game::GetInstance(const string& title, int width, int height) {
-    if ( !instance )
-        instance = new Game(title, width, height);
+void Game::initIMG(Uint32 flags) {
+    int iniciado = IMG_Init(flags);
 
-    return instance;
+    if((iniciado & flags) != flags) {
+        cerr << "Erro ao inicializar imagens: " << IMG_GetError() << endl;
+    }
 }
+
+void Game::initMIX(Uint32 flags) {
+    int iniciado = Mix_Init(flags);
+
+    if((iniciado & flags) != flags
+        || Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024)
+    ){
+        cerr << "Erro ao inicializar Audios: " << Mix_GetError() << endl;
+    }
+}
+
